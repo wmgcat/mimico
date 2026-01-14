ca8a8b40972cf94a7207bd69fd115f6e23ff5897
wmgcat <developer@wmgcat.net>
2026-01-14 23:55:58 +0300

    Added try/catch to fix error shutdown

--- index.ts
+++ index.ts
@@ -14,33 +14,41 @@ const run = () => {
     const bot = new Telegraf(process.env.TOKEN);
     const memory = new Map;
 
-    bot.on("message", (ctx: any) => {
-        if (!ctx?.message?.is_automatic_forward)
-            return;
+    const onChecker = (ctx: any, retry=0) => {
+        try {
+            if (retry > 2)
+                return;
+            
+            if (!ctx?.message?.is_automatic_forward)
+                return;
 
-        // Clear oldest cache
-        for (const [ key, value ] of memory.entries()) {
-            if ((value - Date.now()) <= 0)
-                memory.delete(key);
+            // Clear oldest cache
+            for (const [ key, value ] of memory.entries()) {
+                if ((value - Date.now()) <= 0)
+                    memory.delete(key);
+            }
+
+            const memory_key = ctx.message.media_group_id || ctx.message.message_id;
+
+            if (memory.has(memory_key))
+                return;
+
+            ctx.replyWithPhoto({
+                source: "./src/banner.png"
+            }, {
+                reply_to_message_id: ctx.message.message_id,
+                caption: info
+            });
+
+            memory.set(
+                memory_key,
+                Date.now() + Number(process.env.CLEAR_TIMEOUT || 15000)
+            );
         }
+        catch(err) { onChecker(ctx, retry + 1); }
+    }
 
-        const memory_key = ctx.message.media_group_id || ctx.message.message_id;
-
-        if (memory.has(memory_key))
-            return;
-
-        ctx.replyWithPhoto({
-            source: "./src/banner.png"
-        }, {
-            reply_to_message_id: ctx.message.message_id,
-            caption: info
-        });
-
-        memory.set(
-            memory_key,
-            Date.now() + Number(process.env.CLEAR_TIMEOUT || 15000)
-        );
-    });
+    bot.on("message", (ctx: any) => onChecker(ctx));
 
     bot.launch();
 
